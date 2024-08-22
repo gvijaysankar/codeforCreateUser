b2bf75f58053e4c3cb3a97e6bb9fd7005493d254
gvijaysankar <gvijaysankar@gmail.com>
2024-08-22 18:02:31 +0530

    seleniumCode for Creating User
    seleniumCode for Creating User, 
    Create an autotest that creates an account with a username having 2 characters. I have asked to create a user with 2 characters, I have parameterized to create the user length, I have mentioned the global variable on the user length, create user works successfully for any length unless application accepts it.
    In this code, I have used 2 2 classes , one class is main class and other class is page object class and all the functions related to create user, negative scenaruions in page object class

---
+++ testmainwebdriver.java
@@ -0,0 +1,297 @@
+package practice;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+
+
+/*
+     Create an autotest that creates an account with a username having 2 characters.
+ */
+
+public class  testmainwebdriver{
+	
+	private static  WebDriver driver=null;
+	private static Logger LOGGER = Logger.getLogger(testmainwebdriver.class);
+	String huburl=null;
+	String browser=null;
+	//User length to create
+	int usrlenght=2;
+	@BeforeTest
+	public void beforeTest()
+	{
+	
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		huburl=System.getProperty("grid.hub.url");
+		
+	}
+	
+	public WebDriver getDriver(String browser, String platform,String version) throws MalformedURLException
+	{
+		DesiredCapabilities dc=new DesiredCapabilities();
+		dc.setVersion(version);
+		dc.setBrowserName(DesiredCapabilities.chrome().getBrowserName());
+		dc.setPlatform(Platform.VISTA);
+		dc.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS,true);
+		dc.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
+		if(huburl==null)
+		{
+		driver=new ChromeDriver();
+		}
+		else
+		{
+			driver=new RemoteWebDriver(new URL(huburl),dc);
+		}
+		return driver;
+	}
+	
+	public static void screenShot(WebDriver driver) throws IOException
+	{		
+		String dir=System.getProperty("user.home")+"\\Desktop";
+		File f=new File(dir);
+		String date1=new SimpleDateFormat("ddMMyyy_HHmm").format(Calendar.getInstance().getTime());
+		File f1=new File(f+"/"+date1);
+		if(!f1.exists())
+		{
+			f1.mkdir();
+		}
+		
+		String date2=new SimpleDateFormat("ddMMyyy").format(new Date());
+		
+		File f2=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		File f3=new File(f1+"/"+"date2.png");
+		FileUtils.copyFile(f2, f3);
+		
+		
+		
+	}
+	
+	public static String generateRandomString(int length) throws IOException
+	{		
+		String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	    String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+	    String numbers = "0123456789";
+	    String alphaNumeric = upperAlphabet + lowerAlphabet + numbers;
+	    StringBuilder sb = new StringBuilder();
+	    Random random = new Random();
+	    for(int i = 0; i < length; i++)
+	    {
+	    int index = random.nextInt(alphaNumeric.length());
+	    char randomChar = alphaNumeric.charAt(index);
+	    sb.append(randomChar);
+	    }
+	    String randomString = sb.toString();
+	   return randomString;
+	}
+	public static Map<String,String> updateUserDetails(String userName) throws IOException
+	{
+		Map<String,String> userdetails = new HashMap<String,String>();
+		String password=generateRandomString(9);
+	    String email=generateRandomString(8)+"@gmail.com";
+	    String lastnamee=generateRandomString(8);
+	    userdetails.put("userName",userName);
+	    userdetails.put("emailID",email);
+	    userdetails.put("password",password);
+	    return userdetails;
+	}
+	
+	@Test
+	public void Test1() throws Exception
+	{
+      //Provide the lenght
+	  String userName=generateRandomString(usrlenght);      
+      Map<String,String> gendraldetails = new HashMap<String,String>();
+      int userlenghth=userName.length();
+      try
+      {
+      if(!(userlenghth==0) && !(userName.isEmpty()))
+      {
+    	  Map<String,String> userdetails =updateUserDetails(userName);
+    	  driver= getDriver("CHROME", "Windows 7", "ANY");
+          driver.get("https://try.vikunja.io/register");	
+          testmainwebdriver1 register=new testmainwebdriver1(driver);
+          //Verify the Negitive Scenarions first and check Application is giving proper error messages
+          register.negitiveScenarios(userdetails);
+          
+          String signupurl=register.createAccount(userdetails);
+          Thread.sleep(5000);
+          WebElement username1=driver.findElement(By.xpath(" //span[@class='username']"));
+          if(username1!=null && username1.getText().equalsIgnoreCase(userdetails.get("userName")))
+          {
+        	  LOGGER.info("PASS::SignUp Success");
+           }
+          else
+          {
+        	  LOGGER.info("FAIL::SignUp Success");
+        	  screenShot(driver);
+          }
+          
+      }
+      else
+      {
+    	 LOGGER.info("FAIL::Invalid User Length");
+      }
+      }
+      
+      catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+      
+      finally
+		{
+			 if(driver != null)
+		     {
+		       driver.quit();
+		     }	
+		
+			LOGGER.info("*****************Completed ***************");
+		}
+	 
+      
+      
+      
+      
+      
+     
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	 
+	 
+	 
+	
+	 
+	 
+	 
+	 
+	
+
+	
+	
+	
+	
+	
+	
+	
+    
+ 
+		
+
+		
+		
+		
+		
+       
+		
+		
+		
+	
+	
+		
+		 
+		
+		
+		
+	
+		
+	  
+	 
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	      
+	
+	  
+		
+		
+		
+		
+	
+	
+		
+		
+		
+		
+		
+		
+	    
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+		 
+		 
+		 
+	
+		
+		
+		
+		
+			
+				
+		
+	
+		
+	
+	
+	
+	
+}
+}
+
+
+
---
+++ testmainwebdriver1.java
@@ -0,0 +1,283 @@
+package practice;
+
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+
+public class  testmainwebdriver1 
+{
+private static Logger LOGGER = Logger.getLogger(testmainwebdriver1.class);
+WebDriver driver;	
+WebDriverWait wait;
+
+@FindBy(xpath="//input[@name='username']")	
+WebElement uname1;
+
+@FindBy(xpath="//input[@name='email']")	
+WebElement email1;
+
+@FindBy(xpath="//input[@name='password']")	
+WebElement password1;
+
+
+@FindBy(xpath="//button[contains(@id,'register-submit')]")	
+WebElement signup;
+
+@FindBy(xpath="//div[text()='A user with this email address already exists.']")	
+WebElement userexits1;
+
+@FindBy(xpath="//div[text()='Invalid Data']")	
+WebElement invaliddata1;
+
+public testmainwebdriver1(WebDriver driver)
+{
+	this.driver=driver;
+	PageFactory.initElements(driver,this);
+}
+
+public String createAccount(Map<String,String> userdetails)
+{
+	
+	try
+	{
+	wait=new WebDriverWait(driver,5);
+	
+	//Success Account Creation
+	  LOGGER.info("uname="+ userdetails.get("userName") + " " +"email1="+ userdetails.get("emailID") +""+ "password="+ userdetails.get("password"));
+	(wait).until(ExpectedConditions.visibilityOf(uname1)).sendKeys(userdetails.get("userName"));
+	(wait).until(ExpectedConditions.visibilityOf(email1)).sendKeys(userdetails.get("emailID"));	
+	(wait).until(ExpectedConditions.visibilityOf(password1)).sendKeys(userdetails.get("password"));
+	if(signup.isEnabled())
+	{
+		signup.click();
+	}
+	else
+	{
+	   LOGGER.info("FAIL::CreateAccount button is not enabled");
+	   throw new Exception("CreateAccount button is not enabled");
+	}
+	}
+	
+	catch (Exception e) {
+        e.printStackTrace();
+  }
+	
+	
+	return driver.getCurrentUrl();
+	
+}
+
+public String invalidTestData(String uname,String email,String password,String invaliddata,String existinguser) throws InterruptedException
+{
+	String text="";		
+	try
+	{
+	uname1.sendKeys(uname);
+	email1.sendKeys(email);
+	password1.sendKeys(password);		
+	signup.click();
+	Thread.sleep(5000);
+	if(!(invaliddata.isEmpty()))
+	{
+		text=invaliddata1.getText();
+	}
+	if(!(existinguser.isEmpty()))
+	{
+		text=userexits1.getText();
+	}
+		
+	}
+	catch (Exception e) {
+		LOGGER.info("Issue while validating the Negitive scenarions for User creation");
+        
+  }	
+	
+	return text;
+	
+}
+
+public String missingData(String uname,String email,String password) throws InterruptedException
+{
+	String text="";		
+	try
+	{
+	uname1.sendKeys(uname);
+	email1.sendKeys(email);
+	password1.sendKeys(password);	
+	if(signup.isDisplayed())
+	{
+		text="button disabled";
+	}
+	else
+	{
+		text="button is enabled";
+	}
+		
+	}
+	catch (Exception e) {
+		LOGGER.info("Issue while validating the Negitive scenarions for User creation");
+        
+  }	
+	
+	return text;
+	
+}
+
+/* Function to validate negitive scenarios */
+public void negitiveScenarios(Map<String,String> userdetails) throws InterruptedException
+{
+	String text="";
+   
+	
+	//negitive Scenario with invalid user
+	text=invalidTestData("vijay.", userdetails.get("emailID"), userdetails.get("password"), "invalid", "");
+	if(text.equalsIgnoreCase("Invalid Data"))
+	{
+		LOGGER.info("PASS::Application displaying errror message correctly for Invalid user data");
+	}
+	
+	driver.navigate().refresh();
+	Thread.sleep(3000);
+	
+	//negitive Scenario with password less than 8 characters
+	 text=invalidTestData(userdetails.get("userName"), userdetails.get("emailID"), "pass", "invalid", "");
+	  if(text.equalsIgnoreCase("Invalid Data"))
+	  {
+			LOGGER.info("PASS::Application displaying errror message correctly for Invalid Password");
+	  }
+	  
+	  driver.navigate().refresh();
+	  Thread.sleep(3000);
+	  
+	//negitive Scenario with existing user email
+	  text=invalidTestData(userdetails.get("userName"), "gvijaysankar@gmail.com", userdetails.get("password"), "", "existinguseremail");
+	  if(text.equalsIgnoreCase("A user with this email address already exists."))
+	   {
+			LOGGER.info("PASS::Application displaying errror message correctly when user trying to create user with existing user email");
+	   }
+	  
+	  driver.navigate().refresh();
+	  Thread.sleep(3000);
+	  
+	//negitive Scenario with existing user
+	  text=invalidTestData("gvijaysankar", userdetails.get("emailID"), userdetails.get("password"), "", "existinguser");
+	  if(text.equalsIgnoreCase("A user with this email address already exists."))
+	   {
+			LOGGER.info("PASS::Application displaying errror message correctly when user trying to create user with existing user email");
+	   }
+	  
+	  driver.navigate().refresh();
+	  Thread.sleep(3000);
+	  
+	  //enter only user name and email, check Create button is disabled
+	  text=missingData("gvijaysankar", userdetails.get("emailID"), "");
+	  if(text.equalsIgnoreCase("button disabled"))
+	   {
+			LOGGER.info("PASS::Create button is disabled");
+	   }
+	  else
+	  {
+		  LOGGER.info("FAIL::Create button is enabled");
+	  }
+	  
+	  driver.navigate().refresh();
+	  Thread.sleep(3000);
+}
+
+
+
+
+		
+		
+		
+		
+       
+		
+		
+		
+	
+	
+		
+		 
+		
+		
+		
+	
+		
+	  
+	 
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	      
+	
+	  
+		
+		
+		
+		
+	
+	
+		
+		
+		
+		
+		
+		
+	    
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+		 
+		 
+		 
+	
+		
+		
+		
+		
+			
+				
+		
+	
+		
+	
+	
+	
+	
+}
+
+
+
